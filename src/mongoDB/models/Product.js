0b4ceb38470bc495c79cade371ec8c34e49f6fef
AlexeyTolmachov <taar12sh@gmail.com>
2023-07-06 19:45:36 +0300

    add schema order

--- src/mongoDB/models/Product.js
+++ src/mongoDB/models/Product.js
@@ -1,13 +1,16 @@
+
 const { Schema, model } = require("mongoose");
 
-const Product = new Schema({
+const product_schema = new Schema({
     category: { type: String, required: true },
-    discountPrice: { type: Number },
-    name: { type: String, required: true },
+    discount_price: { type: Number },
+    name_of_product: { type: String, required: true },
     price: { type: Number, required: true },
     unit: { type: String },
     available: { type: Boolean },
     description: { type: String },
     favourite: { type: Boolean },
     img: { type: String }
-})
+}, { collection: 'product' });
+
+module.exports = model("Product", product_schema);
