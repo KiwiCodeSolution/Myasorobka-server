247c62efd23114234f71f12c68c50f8f1fa1e9a1
AlexeyTolmachov <taar12sh@gmail.com>
2023-07-08 17:39:01 +0300

    add product routrs

--- src/routes/product_router.js
+++ src/routes/product_router.js
@@ -2,5 +2,9 @@ const Router = require("express");
 const router = new Router();
 const controller = require("../controller/product_controller");
 
-router.post("/product", controller.create_product);
+router.post("/", controller.create_product);
+router.get('/', controller.get_all_products);
+router.get('/:id', controller.get_product_by_id);
+router.put('/:id', controller.update_product);
+router.delete('/:id', controller.delete_product);
 module.exports = router;
