6685fb7b64c83ed537b0c635ee50aa952a1b934c
AlexeyTolmachov <taar12sh@gmail.com>
2023-07-08 17:39:45 +0300

    add crud product controller

--- src/controller/product_controller.js
+++ src/controller/product_controller.js
@@ -1,4 +1,4 @@
-const product_model = require("../mongoDB/models/Product");
+const Product_model = require("../mongoDB/models/Product");
 
 
 module.exports.create_product = async (req, res) => {
@@ -14,12 +14,79 @@ module.exports.create_product = async (req, res) => {
             favourite,
             img
         } = req.body;
-        const new_product = new product_model({ ...product });
+        const new_product = new Product_model({ ...product });
         const saved_product = await new_product.save();
         res.status(201).json(saved_product);
 
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        res.status(500).json({ error: 'Failed to create product' });
     }
 };
 
+module.exports.get_all_products = async (req, res) => {
+    try {
+        const products = await Product_model.find();
+        res.status(200).json(products);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Failed to fetch products' });
+    }
+};
+
+
+module.exports.get_product_by_id = async (req, res) => {
+    try {
+        const product = await Product_model.findById(req.params.id);
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        res.json(product);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Failed to fetch product' });
+    }
+};
+
+module.exports.update_product = async (req, res) => {
+    try {
+        const old_product = {
+            category,
+            discount_price,
+            name,
+            price,
+            unit,
+            available,
+            description,
+            favourite,
+            img
+        } = req.body;
+        const productId = req.params.id;
+        const updated_product = await Product_model.findByIdAndUpdate(
+            productId,
+            { ...old_product },
+            { new: true }
+        );
+        if (!updated_product) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        res.json(updated_product);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Failed to update product' });
+    }
+};
+
+
+module.exports.delete_product = async (req, res) => {
+    try {
+        const product = await Product_model.findByIdAndRemove(req.params.id);
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        res.json({ message: 'Product deleted successfully' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Failed to delete product' });
+    }
+};
