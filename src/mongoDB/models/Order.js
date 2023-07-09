41e90a46137f7e8f9e591c3ecc489d1ca623e4e8
AlexeyTolmachov <taar12sh@gmail.com>
2023-07-09 16:27:27 +0300

    updated models and add date

--- src/mongoDB/models/Order.js
+++ src/mongoDB/models/Order.js
@@ -1,15 +1,24 @@
 const { Schema, model } = require("mongoose");
 
 const Order_schema = new Schema({
-    order_date: { type: Date, required: true },
-    customer: { type: String, required: true },
+    order_date: {
+        type: Date,
+        required: true,
+        default: () => {
+            const now = new Date();
+            now.setHours(now.getHours() + 3); // Смещение для Киева (UTC+3)
+            return now;
+        }
+    },
+    customer_name: { type: String, required: true },
     phone_number: { type: String, required: true },
     delivery_address: { type: String, required: true },
     total_amount: { type: Number, required: true },
     product: [
         {
             product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
-            quantity: { type: Number, required: true }
+            quantity: { type: Number, required: true },
+
         }
     ],
 
