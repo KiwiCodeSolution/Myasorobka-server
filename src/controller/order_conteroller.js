bfc69845b95c75caa3040d9ff40375dae080c06b
AlexeyTolmachov <taar12sh@gmail.com>
2023-07-09 16:25:54 +0300

    removed try catch

--- src/controller/order_conteroller.js
+++ src/controller/order_conteroller.js
@@ -1,90 +1,47 @@
 const Order = require("../mongoDB/models/Order");
-
+const { NotFound } = require("http-errors");
 
 module.exports.create_order = async (req, res) => {
-    try {
-        const order = {
-            order_date,
-            customer,
-            phone_number,
-            delivery_address,
-            total_amount,
-            product
-        } = req.body;
-
-        const new_order = new Order({ ...order });
-        const saved_order = await new_order.save();
-        res.status(201).json(saved_order);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Failed to create order' });
-    }
+    const new_order = new Order(req.body);
+    const saved_order = await new_order.save();
+    res.status(201).json(saved_order);
 };
 
-
 module.exports.get_all_order = async (req, res) => {
-    try {
-        const orders = await Order.find().populate('product.product');;
-        res.status(200).json(orders);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Failed to fetch orders' });
-    }
+    const orders = await Order.find().populate('product.product');;
+    res.status(200).json(orders);
 };
 
 module.exports.get_order_by_id = async (req, res) => {
-    try {
-        const order = await Order.findById(req.params.id).populate('product.product');;
-        if (!order) {
-            return res.status(404).json({ error: 'Order not found' });
-        }
-        res.json(order);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Failed to fetch order' });
+    const order = await Order.findById(req.params.id).populate('product.product');;
+    if (!order) {
+        throw new NotFound("order not found");
     }
+    res.json(order);
 };
 
 module.exports.update_order = async (req, res) => {
-    try {
-        const order = {
-            order_date,
-            customer,
-            phone_number,
-            delivery_address,
-            total_amount,
-            product
-        } = req.body;
-        const order_id = req.params.id;
-        const updated_order = await Order.findByIdAndUpdate(
-            order_id,
-            { ...order },
-            { new: true }
-        );
-
-        if (!updated_order) {
-            return res.status(404).json({ error: 'Order not found' });
-        }
-
-        res.json(updated_order);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Failed to update order' });
+    const order_id = req.params.id;
+    const updated_order = await Order.findByIdAndUpdate(
+        order_id,
+        req.body,
+        { new: true }
+    );
+    if (!updated_order) {
+        throw new NotFound("order not found");
     }
+    res.json(updated_order);
+
 };
 
 
 module.exports.delete_order = async (req, res) => {
-    try {
-        const order = await Order.findByIdAndRemove(req.params.id);
-        if (!order) {
-            return res.status(404).json({ error: 'Order not found' });
-        }
-        res.json({ message: 'Order deleted successfully' });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Failed to delete order' });
+    const order = await Order.findByIdAndRemove(req.params.id);
+    if (!order) {
+        throw new NotFound("order not found");
     }
+    res.json({ message: 'Order deleted successfully' });
+
 };
 
 
