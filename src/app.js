e576aad385d664812603b34cefbcdccd447b020f
AlexeyTolmachov <taar12sh@gmail.com>
2023-07-11 11:33:59 +0300

    add auth router

--- src/app.js
+++ src/app.js
@@ -3,20 +3,15 @@ const cors = require("cors");
 const app = express();
 const product_router = require("./routes/product_router");
 const order_router = require("./routes/order_router")
+const auth_router = require("./routes/auth_router")
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Драсьте!");
-});
-app.use("/auth", (req, res) => {
-    console.log("auth");
-    res.json({ message: "auth response!", token: 12345 });
-});
-
 app.use("/product", product_router);
 app.use("/order", order_router);
+app.use('/auth', auth_router);
+
 
 app.use((req, res) => {
     res.status(404).json({ message: "Route not found" });
