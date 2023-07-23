4025182fe4f7baf392f8a958690fd684b3fb7d34
Aleksandr-Pod <stdfire@gmail.com>
2023-07-23 23:08:25 +0300

    upload router - get image

--- src/routes/upload_router.js
+++ src/routes/upload_router.js
@@ -5,6 +5,8 @@ const ctrlWrapper = require("../middlewares/ctrlWrapper");
 
 // поставить auth_middleware после тестирования
 router.post("/", img_controller.upload.single("image"), ctrlWrapper(img_controller.upload_img));
+router.get("/"); // добавить контроллер выдачи нужного изображения
+
 module.exports = router;
 
 
